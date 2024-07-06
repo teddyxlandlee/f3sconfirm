29abedbb5263efefdba53effe068f881d6340385
Teddy Li <teddyxlandlee@hotmail.com>
2024-07-06 14:49:02 +0000

    1.0.2: support 1.21

--- src/main/java/xland/mcmod/f3sconfirm/F3SConfirmClient.java
+++ src/main/java/xland/mcmod/f3sconfirm/F3SConfirmClient.java
@@ -1,9 +1,12 @@
 package xland.mcmod.f3sconfirm;
 
+import com.google.common.base.Suppliers;
 import com.mojang.blaze3d.platform.TextureUtil;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.version.VersionPredicate;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -14,14 +17,16 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
+import java.lang.invoke.*;
 import java.nio.file.Path;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public class F3SConfirmClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientCommandRegistrationCallback.EVENT.register(new Identifier("f3sconfirm", "default"), (dispatcher, registryAccess) -> {
+        ClientCommandRegistrationCallback.EVENT.register(ofId("f3sconfirm", "default"), (dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommandManager.literal("f3sconfirm")
                     .executes(context -> {
                         final FabricClientCommandSource commandSource = context.getSource();
@@ -57,4 +62,46 @@ public class F3SConfirmClient implements ClientModInitializer {
                 .append(text)
         );
     }
+
+    private static final Supplier<MethodHandle> OF_IDENTIDIER = Suppliers.memoize(() -> {
+    	var lookup = MethodHandles.lookup();
+    	var methodType = MethodType.methodType(Identifier.class, String.class, String.class);
+    	
+    	boolean useIdentifierOf;
+    	try {
+    	    var mcVersion = FabricLoader.getInstance().getModContainer("minecraft").orElseThrow().getMetadata().getVersion();
+    	    useIdentifierOf = VersionPredicate.parse(">=1.21-alpha.24.21.a").test(mcVersion);
+    	} catch (Exception e) {
+    		throw new RuntimeException(e);
+    	}
+
+    	if (!useIdentifierOf) {
+    	    try {
+    	        return lookup.findConstructor(Identifier.class, methodType.changeReturnType(void.class));
+    	    } catch (Exception e) {
+    	    	throw new RuntimeException(e);
+    	    }
+    	}
+
+    	String methodName = FabricLoader.getInstance().getMappingResolver().mapMethodName(
+    	    "intermediary",
+    		"net.minecraft.class_2960",
+    		"method_60655",
+    		"(Ljava/lang/String;Ljava/lang/String;)Lnet/minecraft/class_2960;"
+    	);
+    	try {
+    	    return lookup.findStatic(Identifier.class, methodName, methodType);
+    	} catch (Exception e) {
+    		throw new RuntimeException(e);
+    	}
+    });
+
+    public static Identifier ofId(String namespace, String path) {
+        try {
+    	    return (Identifier) OF_IDENTIDIER.get().invokeExact(namespace, path);
+    	} catch (Throwable t) {
+    		throw new RuntimeException(t);
+    	}
+    }
+
 }
